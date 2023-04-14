7f8bfa915cb19fd2a45056c06463a78d18e1871b
Gabriela Teixeira <gabriela.tex91@gmail.com>
2023-04-14 13:36:27 -0300

    Resolvendo o terceiro exercicio de vetores

---
+++ exercicios-dio/src/main/java/br/com/dio/exercicios/arrays/Ex3.java
@@ -0,0 +1,28 @@
+package br.com.dio.exercicios.arrays;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/*
+Faça um programa que leia 20 números aleatórios entre (0 e 100) armazene-os em um vetor.
+Ao final mostre os números e seus sucessores
+ */
+public class Ex3 {
+    public static void main(String[] args) {
+
+        Random random = new Random();
+
+        int [] vetor = new int[20];
+        int i = 0;
+
+        for(i = 0; i < vetor.length; i++){
+            int numero = random.nextInt(100);
+            vetor[i] = numero;
+        }
+
+        for(int numero : vetor){
+            System.out.println("Números aleatórios: " + numero + " " + "Números sucessores: " + (numero+1));
+
+        }
+    }
+}
